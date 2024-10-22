0581fad5e95cdc012f86631b6f86d1a76b4f0759
MarchinovVladimir <114493729+MarchinovVladimir@users.noreply.github.com>
2024-10-22 08:05:40 +0300

    only removed

--- tests/ui/add-grade.test.js
+++ tests/ui/add-grade.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Check add grades page', async ({ page }) => {
+test('Check add grades page', async ({ page }) => {
     await page.goto('http://localhost:8090/My-Grades');
     const form = await page.$('ul');
     expect(form).toBeTruthy();
